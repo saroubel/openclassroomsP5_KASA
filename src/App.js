9ae991f8724f7d6a38a97e47cadbbbb15dd9de84
saroubel <bellaf.sara@gmail.com>
2024-09-03 23:54:40 +0200

    Add react router and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-
-import logo from './logo_test.png';
+import React from 'react'; 
 import './App.css';
+//importer les pages
+import Home from "./pages/home";
+import About from "./pages/about";
+import Error from "./pages/error";
+import LogementDetail from "./pages/logementDetail";
+//importer react-router-dom pour les liens de navigation 
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" /> 
-      </header>   
-      </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Navigate replace to="/accueil" />} />
+        <Route path="/accueil" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<LogementDetail />} /> 
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
