6c99716edef807ec5cabe65722a1a98f618dbfc6
saroubel <bellaf.sara@gmail.com>
2024-09-06 01:19:49 +0200

    Update style error

---
+++ src/pages/logementDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+//On ajoute nos composants
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function LogementDetail() {
+    return (
+        <div>
+            <Header />  
+            <Footer />          
+        </div>
+    );
+}
+
+export default LogementDetail;
