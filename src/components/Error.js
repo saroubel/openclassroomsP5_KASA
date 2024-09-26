eb9aa5e3c74df15179eb600c7834824eeaa00cbd
saroubel <bellaf.sara@gmail.com>
2024-09-26 19:10:23 +0200

    Create Partial files & Variables

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,10 @@
-import ErrorImg from "../assets/error/404.png";
 import { Link } from "react-router-dom"; //Composant pour les liens de navigation
 
 const ErrorContent = () => {
     return (
         <>
         <div className="error_content">
-            <img className = "error_img" src={ErrorImg} alt="pic page not found" />
+            <h1 className="error_number">404</h1>
             <p className="error_text">Oups! La page que vous demandez n'existe pas.</p>
             <Link className = "error_link_home" to="/">Retourner sur la page d'accueil</Link>
         </div>
